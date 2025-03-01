b6e02a791ea99d320d00ce795f97766695e4be0e
Michael Ball <aaronhunter@live.com>
2025-03-01 15:30:10 -0600

    class updates

--- src/main/java/skaro/pokeapi/resource/evolutionchain/EvolutionChain.java
+++ src/main/java/skaro/pokeapi/resource/evolutionchain/EvolutionChain.java
@@ -6,24 +6,40 @@ import skaro.pokeapi.resource.item.Item;
 public class EvolutionChain {
 
 	private Integer id;
-	private NamedApiResource<Item> item;
+	private NamedApiResource<Item> babyTriggerItem;
 	private ChainLink chain;
-	
+
+	/** Gets the id for this evolution chain resource */
 	public Integer getId() {
 		return id;
 	}
+	/** Sets the id for this evolution chain resource */
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	public NamedApiResource<Item> getItem() {
-		return item;
+
+	/**
+	 * Gets the item that a Pokemon must be holding
+	 * when leveling up to evolve into a Pokemon in
+	 * this evolution chain
+	 */
+	public NamedApiResource<Item> getBabyTriggerItem() {
+		return babyTriggerItem;
 	}
-	public void setItem(NamedApiResource<Item> item) {
-		this.item = item;
+	/**
+	 * Sets the item that a Pokemon must be holding
+	 * when leveling up to evolve into a Pokemon in
+	 * this evolution chain
+	 */
+	public void setBabyTriggerItem(NamedApiResource<Item> babyTriggerItem) {
+		this.babyTriggerItem = babyTriggerItem;
 	}
+
+	/** Gets the chain of Pokemon species that make up this evolution chain */
 	public ChainLink getChain() {
 		return chain;
 	}
+	/** Sets the chain of Pokemon species that make up this evolution chain */
 	public void setChain(ChainLink chain) {
 		this.chain = chain;
 	}
