cd6e87c3c1881ffc21de63cf4e44d4ce0ad6aad1
Michael Ball <aaronhunter@live.com>
2025-02-28 21:06:59 -0600

    javadocs

--- src/main/java/skaro/pokeapi/query/PageQuery.java
+++ src/main/java/skaro/pokeapi/query/PageQuery.java
@@ -4,22 +4,28 @@ public class PageQuery {
 
 	private Integer limit;
 	private Integer offset;
-	
+
+	/** All args constructor **/
 	public PageQuery(int limit, int offset) {
-		this.limit = limit;
-		this.offset = offset;
+		setLimit(limit);
+		setOffset(offset);
 	}
-	
+
+	/** Gets the limit **/
 	public Integer getLimit() {
 		return limit;
 	}
-	public void setLimit(Integer limit) {
+	/** Sets the limit **/
+	private void setLimit(Integer limit) {
 		this.limit = limit;
 	}
+
+	/** Gets the offset **/
 	public Integer getOffset() {
 		return offset;
 	}
-	public void setOffset(Integer offset) {
+	/** Sets the offset **/
+	private void setOffset(Integer offset) {
 		this.offset = offset;
 	}
 	
