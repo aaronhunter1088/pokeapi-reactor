db03de5b865f0df99979cad070661bd39165de3f
Michael Ball <aaronhunter@live.com>
2025-03-03 01:32:21 -0600

    NamedApiResource extends apiResource

--- src/main/java/skaro/pokeapi/resource/NamedApiResource.java
+++ src/main/java/skaro/pokeapi/resource/NamedApiResource.java
@@ -1,21 +1,14 @@
 package skaro.pokeapi.resource;
 
-public class NamedApiResource<T extends PokeApiResource> {
+public class NamedApiResource<T extends PokeApiResource> extends ApiResource<T> {
 
 	private String name;
-	private String url;
 	
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
-	}
-	public String getUrl() {
-		return url;
-	}
-	public void setUrl(String url) {
-		this.url = url;
 	}
 	
 }
