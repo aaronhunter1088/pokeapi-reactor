0d52672ab9c163a9f043367f9544e9823d6014fe
Michael Ball <aaronhunter@live.com>
2025-03-10 00:17:27 -0500

    import cleanup

--- src/main/java/skaro/pokeapi/resource/nature/Nature.java
+++ src/main/java/skaro/pokeapi/resource/nature/Nature.java
@@ -3,7 +3,6 @@ package skaro.pokeapi.resource.nature;
 import java.util.List;
 import java.util.Objects;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import skaro.pokeapi.resource.*;
 import skaro.pokeapi.resource.berryflavor.BerryFlavor;
