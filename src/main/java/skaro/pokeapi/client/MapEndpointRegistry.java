bbaf12a10535804a4a16bf50e4b40e35b028578a
Michael Ball <aaronhunter@live.com>
2025-12-21 03:27:13 -0600

    convert MapEndpointRegistry to record

--- src/main/java/skaro/pokeapi/client/MapEndpointRegistry.java
+++ src/main/java/skaro/pokeapi/client/MapEndpointRegistry.java
@@ -6,18 +6,19 @@ import java.util.Collections;
 import java.util.Map;
 
 /**
- * Implementation of PokeApiEndpointRegistry that uses a provided map
+ * Implementation of PokeApiEndpointRegistry that
+ * uses a provided map.
+ * Existed with @author skaro @since 0.0.1-SNAPSHOT
+ * as a class until this version.
  *
- * @author skaro
- * @since 0.0.1-SNAPSHOT
+ * @author michael ball
+ * @since 2.0.0
  */
-public class MapEndpointRegistry implements PokeApiEndpointRegistry {
+public record MapEndpointRegistry (
+        Map<Class<?>, String> endpoints
+) implements PokeApiEndpointRegistry {
 
-    private final Map<Class<?>, String> endpoints;
-
-    public MapEndpointRegistry(Map<Class<?>, String> endpoints) {
-        this.endpoints = Collections.unmodifiableMap(endpoints);
-    }
+    public MapEndpointRegistry(Map<Class<?>, String> endpoints) { this.endpoints = Collections.unmodifiableMap(endpoints); }
 
     @Override
     public <T extends PokeApiResource> String getEndpoint(Class<T> resource) {
