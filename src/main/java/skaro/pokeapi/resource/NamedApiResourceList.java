aead8470794071e7d28207434062cd10dbd6ed83
Michael Ball <aaronhunter@live.com>
2025-12-25 10:57:49 -0600

    convert NamedApiResourseList to record

--- src/main/java/skaro/pokeapi/resource/NamedApiResourceList.java
+++ src/main/java/skaro/pokeapi/resource/NamedApiResourceList.java
@@ -6,89 +6,22 @@ import java.util.List;
 
 /**
  * NamedApiResponseList, the response from
- * calling endpoints in the registry
+ * calling endpoints in the registry.
+ * Existed with @author skaro @since 0.0.1-SNAPSHOT
+ * as a class until this version.
  *
- * @author skaro
- * @since 0.0.1-SNAPSHOT
+ * @author michael ball
+ * @since 2.0.0
  */
-public class NamedApiResourceList<T extends PokeApiResource> implements PokeApiResource {
+public record NamedApiResourceList<T extends PokeApiResource> (
+        Integer count,
+        String next,
+        String previous,
+        List<NamedApiResource<T>> results
+) implements PokeApiResource {
 
-    private Integer count;
-    private String next;
-    private String previous;
-    private List<NamedApiResource<T>> results;
-
-    /**
-     * Get the count
-     *
-     * @return the total number of results
-     */
-    public Integer getCount() {
-        return count;
-    }
-
-    /**
-     * Set the count
-     *
-     * @param count the number of results
-     */
-    public void setCount(Integer count) {
-        this.count = count;
-    }
-
-    /**
-     * Get the next page
-     *
-     * @return the url to the next page
-     */
-    public String getNext() {
-        return next;
-    }
-
-    /**
-     * Set the next page
-     *
-     * @param next the url to the next page
-     */
-    public void setNext(String next) {
-        this.next = next;
-    }
-
-    /**
-     * Get the previous page
-     *
-     * @return the url to the previous page
-     */
-    public String getPrevious() {
-        return previous;
-    }
-
-    /**
-     * Set the previous page
-     *
-     * @param previous the url to the previous page
-     */
-    public void setPrevious(String previous) {
-        this.previous = previous;
-    }
-
-    /**
-     * Get the results
-     *
-     * @return list of {@link NamedApiResource}
-     */
-    public List<NamedApiResource<T>> getResults() {
-        return results;
-    }
-
-    /**
-     * Set the results
-     *
-     * @param results list of {@link NamedApiResource}
-     */
-    public void setResults(List<NamedApiResource<T>> results) {
-        this.results = results;
-    }
+    /* No-args constructor */
+    public NamedApiResourceList() { this(0, null, null, List.<NamedApiResource<T>>of()); }
 
     @Override
     @JsonIgnore
